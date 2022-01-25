85f300bba9412256c5760a8b497f4196caf2321a
nabangi <alvaronabangi@gmail.com>
2022-01-25 12:59:23 +0300

    update configs

--- Alpine/configs.sh
+++ Alpine/configs.sh
@@ -4,10 +4,12 @@
 vi /etc/ssh/sshd_config
 
 cat > /etc/apk/repositories << EOF; $(echo)
- http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/main
- http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/community
- http://dl-cdn.alpinelinux.org/alpine/edge/testing
- EOF
+
+http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/main
+http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/community
+http://dl-cdn.alpinelinux.org/alpine/edge/testing
+
+EOF
 
  apk updated
  
